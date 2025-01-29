7c0dadeb81151bab796e3dea4c961746534a7a2f
tesla-a-n <143447224+tesla-a-n@users.noreply.github.com>
2025-01-29 15:45:06 -0700

    Update code.js

--- code.js
+++ code.js
@@ -1,3 +1,5 @@
-function fib(n) {
-    return n;
+function fib(n, res = [0, 1]) { //takes n and an array with the first two elements being 0 and 1
+    if (n < res.length) return res.slice(0, n + 1); //base case checks if the array has the correct amount of elements 
+    res.push(res[res.length - 1] + res[res.length - 2]); //calculates and pushes the next fibonacci number onto the array 
+    return fib(n, res); 
 }
